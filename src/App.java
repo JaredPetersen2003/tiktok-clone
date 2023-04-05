17b1751a76103332b6498914062cb0555220ede3
JaredPetersen2003 <jared.petersen9@gmail.com>
2023-04-05 11:19:19 +0200

    Added user menu

--- src/App.java
+++ src/App.java
@@ -5,11 +5,74 @@ import java.io.FileNotFoundException;
 public class App {
 
     private static UserTree Users;
+
     public static void main(String[] args) throws Exception {
-        Users = new UserTree();
-        loadDataSet();
-        Users.inOrderTraversal(Users.root);
         
+        Users = new UserTree();
+        
+        int choice;
+
+        while (choice != 8){
+            System.out.println("Choose an action from the menu:");
+            System.out.println("1. Find the profile description for a given account");
+            System.out.println("2. List all accounts");
+            System.out.println("3. Create an account");
+            System.out.println("4. Delete an account");
+            System.out.println("5. Display all posts for a single account");
+            System.out.println("6. Add a new post for an account");
+            System.out.println("7. Load a file of actions from disk and process this");
+            System.out.println("8. Quit");
+            System.out.println("Enter your choice");
+            
+            Scanner scan = new Scanner(System.in);
+            int choice = scan.nextLine():
+            switch (choice) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    Users.inOrderTraversal(Users.root);
+                    break;
+                case 3:
+                    addUser()
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    loadDataSet();
+                    break;
+                case 8:
+                    
+                    break;
+            
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }
+    }
+
+
+    private static void addUser(){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter User Name");
+        String userName = scan.nextLine();
+        System.out.println("Enter Profile Description");
+        String profileDescription = scan.nextLine();
+
+        Users.insert(new User(profileDescription, userName));
+        System.out.println("User Added!")        
+
     }
     
     private static void loadDataSet(){
