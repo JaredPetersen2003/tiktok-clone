c446521e8505264dcacdef9fbce4da49a464960c
JaredPetersen2003 <jared.petersen9@gmail.com>
2023-04-18 08:39:11 +0200

    Added user posts functionality

--- src/Post.java
+++ src/Post.java
@@ -4,14 +4,33 @@ public class Post implements Comparable<Post>{
     private String  video;
     private int numLikes;
 
+    
+    /**
+     * The Post function is a constructor that creates an instance of the Post class.
+     * 
+     *
+     * @param String title Set the title of the post
+     * @param String  video Set the video field
+     * @param int numLikes Set the number of likes for a post
+     *
+     * @return The title of the video
+     */
     public Post(String title, String  video, int numLikes){
         this.title = title;
         this.video = video;
         this.numLikes = numLikes;
     }
 
+    
+    /**
+     * The toString function returns a string representation of the object.
+     * 
+     *
+     *
+     * @return The title, video and number of likes
+     */
     public String toString(){
-        return "post content";
+        return "Title: " + title + "\nVideo: " + video + "\nNumber of Likes: " + Integer.toString(numLikes);
     }
 
     public int getNumLikes(){
